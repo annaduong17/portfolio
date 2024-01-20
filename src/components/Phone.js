6df18dfa8f09081e85b666e3b2c80e8aa62f35d2
Anna Duong <aduong9417@gmail.com>
2024-01-20 14:02:32 -0800

    add content to Phone

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -1,6 +1,24 @@
 function Phone() {
   return (
-    <div>Phone</div>
+    <div className="phone">
+      <div className="phone-display">
+        <img src="/images/avatar.jpeg" alt="avatar" />
+        <h2>ANNA DUONG</h2>
+        <p>Currently learning Next.js</p>
+      </div>
+
+      <div className="phone-control">
+        <button>
+          <img src="/images/icons/icon-previous.svg" alt="previous icon" />
+        </button>
+        <button>
+          <img src="/images/icons/icon-play.svg" alt="play icon" />
+        </button>
+        <button>
+          <img src="/images/icons/icon-next.svg" alt="next icon" />
+        </button>
+      </div>
+    </div>
   );
 }
 
