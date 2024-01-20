a6034a1c3bd147ac3a633683eacf6fe8c9032699
Anna Duong <aduong9417@gmail.com>
2024-01-20 15:30:37 -0800

    add content to Details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,6 +1,21 @@
-function Details() {
+function Details({ type, name, description, features }) {
+  let classes;
+  type === "experience" ? classes = "experience" : classes = "project";
+
+  const renderedItems = features.map((feature, index) => {
+    return (
+      <li key={index} className="feature">{feature}</li>
+    );
+  })
+
   return (
-    <div>Details</div>
+    <div className={classes}>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <ul className="features">
+        {renderedItems}
+      </ul>
+    </div>
   );
 }
 
