2f72eff34301d366666b27e8643f497352676a21
Anna Duong <aduong9417@gmail.com>
2024-01-21 01:05:13 -0800

    style Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,29 +3,31 @@ import { NavLink } from 'react-router-dom';
 function Home() {
   return (
     <div className="home">
+
       <div className="home-heading">
-        <h1>Hello there, I'm Anna.</h1>
+        <h1>Hello there, I'm <span>Anna</span>.</h1>
         <h2>FRONT END DEVELOPER</h2>
       </div>
 
-      <div className="home-socials">
-        <img width={50} height={50} src="/images/icons/icon-github.svg" alt="github icon" />
-        <img width={50} height={50} src="/images/icons/icon-linkedin.svg" alt="linkedin icon" />
-      </div>
+      <div className='socials-description'>
+        <div className="home-socials">
+          <img width={30} height={30} src="/images/icons/icon-github.svg" alt="github icon" />
+          <img width={35} height={25} src="/images/icons/icon-linkedin.svg" alt="linkedin icon" />
+        </div>
 
-      <div className="home-description">
-        <p>I find joy in turning goals and visions into aesthetically pleasing user interfaces with satisfying user experience. Let me know how I can help.</p>
+        <div className="home-description">
+          <p>I find joy in turning goals and visions into aesthetically pleasing user interfaces with satisfying user experience. Let me know how I can help.</p>
+        </div>
       </div>
-
       <div className="home-buttons">
-        <NavLink to="/contact" className="btn primary-btn">HIRE ME</NavLink>
-        <a download href="/files/anna-duong-resume.pdf" className="btn secondary-btn">DOWNLOAD RESUME</a>
+        <NavLink to="/contact" className="btn primary-btn">Hire Me</NavLink>
+        <a download href="/files/anna-duong-resume.pdf" className="btn secondary-btn">Download Resume</a>
       </div>
 
-      <div>
+      <div className='view-portfolio-container'>
         <NavLink to="/portfolio">
           <p>View my portfolio</p>
-          <img src="/images/icons/icon-arrow-right.svg" alt="right arrow icon" />
+          <img width={30} height={30} src="/images/icons/icon-arrow-right.svg" alt="right arrow icon" />
         </NavLink>
       </div>
     </div>
