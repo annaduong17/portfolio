5dfee928a76a50ee0311d2510999002372298532
Anna Duong <aduong9417@gmail.com>
2024-01-20 16:43:26 -0800

    add content to ContactForm

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,21 @@
+import Button from './Button';
+
 function ContactForm() {
   return (
     <div>
-      Contact Form
+      <h2>Contact Me</h2>
+      <p>I am fluent in English and Vietnamese. Feel free to communicate with me in either language.</p>
+
+      <form action="">
+        <label htmlFor="name">Full Name</label>
+        <input id="name" type="text" />
+        <label htmlFor="email">Email</label>
+        <input id="email" type="email" />
+        <label htmlFor="message">Message</label>
+        <textarea id="message" type="text"></textarea>
+
+        <Button>Submit</Button>
+      </form>
     </div>
   );
 }
