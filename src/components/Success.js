6c57dd5f6cbaa0ec2be66381f1b39702bd564c6d
Anna Duong <aduong9417@gmail.com>
2024-01-20 16:49:25 -0800

    add Success component

---
+++ src/components/Success.js
@@ -0,0 +1,10 @@
+function Success() {
+  return (
+    <div>
+      <h2>Thank you!</h2>
+      <p>I have received your message and will get back to you as soon as possible.</p>
+    </div>
+  )
+}
+
+export default Success;
