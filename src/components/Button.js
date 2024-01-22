441d90773d88ac95759962234d0073452e5db2c7
Anna Duong <aduong9417@gmail.com>
2024-01-21 20:31:51 -0800

    open new tab when clicking on a button to visit different site

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 function Button({ className, children, link }) {
 
   return (
-    <a className="link" href={link}>
+    <a className="link" href={link} target="_blank" rel="noreferrer">
       <button className={className}>
         {children}
       </button>
