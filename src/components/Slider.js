4b4e24c5bd074c8b14a3bd1870d495e229a12852
Anna Duong <aduong9417@gmail.com>
2024-01-22 19:51:24 -0800

    implement Slider logic

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,35 +1,67 @@
+import { useState } from 'react';
+
 function Slider({ type, name, images }) {
-  const renderedImages = images.map((image, index) => {
+  const [ index, setIndex ] = useState(0);
+  const [ selectedIndex, setSelectedIndex ] = useState(0);
+
+  const handleDotClick = (i) => {
+    setIndex(i);
+    setSelectedIndex(i);
+  }
+
+  const handleImagePrevious = () => {
+    setSelectedIndex(prev => {
+      return prev === 0 ? images.length - 1 : prev - 1;
+    });
+
+    setIndex(prev => {
+      return prev === 0 ? images.length - 1 : prev - 1;
+    });
+  };
+
+  const handleImageNext = () => {
+    setSelectedIndex(prev => {
+      return prev === images.length - 1 ? 0 : prev + 1;
+    });
+
+    setIndex(prev => {
+      return prev === images.length - 1 ? 0 : prev + 1;
+    });
+  };
+
+  const renderedImages = images.map((image, i) => {
     return (
       <img 
-        key={index}
+        key={i}
         src={image} 
         alt={name}
+        style={{ translate: `${-100 * index}%` }}
       />
     );
   });
 
-  const renderedDots = images.map((image, index) => {
+  const renderedDots = images.map((image, i) => {
+    const isSelected = i === selectedIndex;
+
     return (
-      <img 
-        key={index}
-        src="/images/icons/icon-dot.svg"
-        alt="dot icon"
-        width={30}
-      />
+      <button
+        key={i}
+        className={`slider-dot${isSelected ? ' cherry' : ''}`}
+        onClick={() => handleDotClick(i)}>
+    </button>
     );
   });
 
   return (
     <div className="slider">
       <div className="img-btn-container">
-        <button>
+        <button onClick={() => handleImagePrevious()}>
           <img src="/images/icons/icon-arrow-left-circle.svg" alt="arrow left icon" />
         </button>
         <div className="images">
           {renderedImages}
         </div>
-        <button>
+        <button onClick={handleImageNext}>
           <img src="/images/icons/icon-arrow-right-circle.svg" alt="arrow right icon" />
         </button>
       </div>
