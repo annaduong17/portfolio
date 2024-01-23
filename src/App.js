575f40d2ef2b20a21c2c4a9650fa5b1cab6af32a
Anna Duong <aduong9417@gmail.com>
2024-01-23 09:34:45 -0800

    toggle navbar styles and make sticky

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
@@ -6,12 +7,29 @@ import Contact from './components/Contact';
 import NotFound from './components/NotFound';
 
 function App () {
+  const [ scrolled, setScrolled ] = useState(false);
+  const navbarRef = useRef();
+
+  useEffect(() => {
+    const handleScroll = window.addEventListener('scroll', () => {
+      setScrolled(true);
+      
+      if (window.scrollY === 0) {
+        setScrolled(false);
+      }
+    });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [scrolled]);
+
   return(
     <BrowserRouter>
       <div className='app-container'>
+      
         <div className='app'>
-          <Navbar />
-
+        <Navbar scrolled={scrolled} ref={navbarRef} />
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/portfolio' element={<PortfolioPage />} />
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import { NavLink } from 'react-router-dom';
 
-function Navbar() {
+function Navbar({ scrolled }) {
   return (
-    <nav className='navbar'>
+    <nav className={`navbar ${scrolled ? "white-bg" : ""}`}>
       <NavLink className="navlink" to="/">HOME</NavLink>
       <NavLink className="navlink" to="/portfolio">PORTFOLIO</NavLink>
       <NavLink className="navlink" to="/contact">CONTACT</NavLink>
