5dab440c70fa26bf842beb9bb988ab3702c3cb2a
Anna Duong <aduong9417@gmail.com>
2024-01-27 21:27:30 -0800

    increase menu link click area and lowercase toggle text

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,14 +7,14 @@ function Menu({ toggleMenu }) {
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/></svg>
       </button>
       <ul>
-        <li onClick={toggleMenu}>
-          <NavLink to='/'>HOME</NavLink>
+        <li>
+          <NavLink onClick={toggleMenu} to='/'>HOME</NavLink>
         </li>
-        <li onClick={toggleMenu}>
-          <NavLink to='/portfolio'>PORTFOLIO</NavLink>
+        <li>
+          <NavLink onClick={toggleMenu} to='/portfolio'>PORTFOLIO</NavLink>
         </li>
-        <li onClick={toggleMenu}>
-          <NavLink to='/contact'>CONTACT</NavLink>
+        <li>
+          <NavLink onClick={toggleMenu} to='/contact'>CONTACT</NavLink>
         </li>
       </ul>
     </div>
