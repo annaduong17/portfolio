37883b66bc1d84da1451ed270d4cee9da92d1c2b
Anna Duong <aduong9417@gmail.com>
2024-02-04 16:52:26 -0800

    change * to portfolio url

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://www.anna-duong.com');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Credentials', true);
